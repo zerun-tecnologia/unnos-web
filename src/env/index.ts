78b58faf190909965ea00e30a810ee0e815e77de
Pedro Veras <veraspedro1608@gmail.com>
2025-03-08 15:05:58 -0300

    feat: ✨ env configuration file

---
+++ src/env/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  DB_USER: z.string(),
+  DB_PASS: z.string(),
+  DB_NAME: z.string(),
+  DB_HOST: z.string(),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
